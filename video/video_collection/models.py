6f8ec94451b6f768b4a6a020ef909a6906a089cd
Marty M <github@moontronic.com>
2023-04-28 15:41:10 -0600

    finished the second video. didnt make a commit for the end of the first video... oh well

--- video/video_collection/models.py
+++ video/video_collection/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=500)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .forms import VideoForm
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    app_name = 'Retro Tech Videos'
+    return render(request, 'video_collection/home.html', {'app_name':app_name})
+
+
+def add(request):
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save()
+            messages.info(request, 'New video saved!')
+        
+            # todo show done message or redirect
+        else:
+            messages.warning(request, 'Please check data entered!')
+            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+    
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
